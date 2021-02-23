552fc66bd3eca956cb994cab06e57d719c1861e9
samueljrz <samuelevangelista.ti@gmail.com>
2021-02-23 15:17:27 -0300

    CountDown component and page icon

---
+++ moveit-next/src/components/CountDown.tsx
@@ -0,0 +1,53 @@
+import { useState, useEffect } from 'react'
+import styles from '../styles/components/CountDown.module.css'
+
+export function CountDown() {
+  const [time, setTime] = useState(25*60)
+  const [active, setActive] = useState(false);
+  
+  const minutes = Math.floor(time / 60);
+  const seconds = time % 60;
+
+  // Diego way
+  // const [minuteLeft, minuteRight] = String(minutes).padStart(2, '0').split('')
+  // const [secondLeft, secondRight] = String(seconds).padStart(2, '0').split('')
+
+  // My way
+  const [minuteLeft, minuteRight] = String(minutes).length > 1 ? String(minutes).split('') : ['0', String(minutes)]
+  const [secondLeft, secondRight] = String(seconds).length > 1 ? String(seconds).split('') : ['0', String(seconds)]
+
+  function startCountDown() {
+    setActive(true);
+  }
+
+  useEffect(() => {
+    if(active &&  time > 0) {
+      setTimeout(() => {
+        setTime(time - 1);
+      }, 1000);
+    }
+  }, [active, time])
+
+  return (
+    <div>
+      <div className={styles.countDownContainer}>
+        <div>
+          <span>{minuteLeft}</span>
+          <span>{minuteRight}</span>
+        </div>
+        <span>:</span>
+        <div>
+          <span>{secondLeft}</span>
+          <span>{secondRight}</span>
+        </div>
+      </div>
+      <button 
+        type="button" 
+        className={styles.countDownButton}
+        onClick={startCountDown}
+      >
+        Iniciar um ciclo
+      </button>
+    </div>
+  )
+}
--- moveit-next/src/pages/index.tsx
+++ moveit-next/src/pages/index.tsx
@@ -1,17 +1,25 @@
 import { ExperienceBar } from '../components/ExperienceBar'
 import { Profile } from '../components/Profile'
 import { CompletedChallenges } from '../components/CompletedChallenges'
+import { CountDown } from '../components/CountDown'
+
+import Head from 'next/head'
 
 import styles from '../styles/pages/Home.module.css'
+
 
 export default function Home() {
   return (
     <div className={styles.container}>
+      <Head>
+        <title>Inicio | Move.It</title>
+      </Head>
       <ExperienceBar />
       <section>
         <div>
           <Profile />
           <CompletedChallenges />
+          <CountDown />
         </div>
         <div>
 
