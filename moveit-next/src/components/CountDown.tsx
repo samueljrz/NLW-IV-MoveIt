e4a5c22990877dee00bcb15e633b041c8bb336e8
samueljrz <samuelevangelista.ti@gmail.com>
2021-02-23 15:53:18 -0300

    açao de pausar e retomar o contador

--- moveit-next/src/components/CountDown.tsx
+++ moveit-next/src/components/CountDown.tsx
@@ -3,7 +3,8 @@ import styles from '../styles/components/CountDown.module.css'
 
 export function CountDown() {
   const [time, setTime] = useState(25*60)
-  const [active, setActive] = useState(false);
+  const [active, setActive] = useState(false)
+  const [text, setText] = useState('Iniciar um ciclo')
   
   const minutes = Math.floor(time / 60);
   const seconds = time % 60;
@@ -17,13 +18,19 @@ export function CountDown() {
   const [secondLeft, secondRight] = String(seconds).length > 1 ? String(seconds).split('') : ['0', String(seconds)]
 
   function startCountDown() {
-    setActive(true);
+    if(!active) {
+      setActive(true)
+      setText('Pausar o ciclo')
+    }else {
+      setActive(false)
+      setText('Retorna o ciclo')
+    }
   }
 
   useEffect(() => {
     if(active &&  time > 0) {
       setTimeout(() => {
-        setTime(time - 1);
+        setTime(time - 1)
       }, 1000);
     }
   }, [active, time])
@@ -46,7 +53,7 @@ export function CountDown() {
         className={styles.countDownButton}
         onClick={startCountDown}
       >
-        Iniciar um ciclo
+        {text}
       </button>
     </div>
   )
