32ff0f242c09981210f1580ab083f38752c90ad0
samueljrz <samuelevangelista.ti@gmail.com>
2021-02-23 13:43:26 -0300

    fix word error and style specific component

---
+++ moveit-next/src/components/ExperienceBar.tsx
@@ -0,0 +1,14 @@
+import styles from '../styles/components/ExperienceBar.module.css'
+
+export function ExperienceBar() {
+  return(
+    <header className={styles.experienceBar}>
+      <span>0 xp</span>
+      <div>
+        <div style={{width: '60%'}}></div>
+        <span className={styles.currentExperience} style={{ left: '60%'}}>360 xp</span>
+      </div>
+      <span>600 xp</span>
+    </header>
+  );
+}
--- moveit-next/src/pages/index.tsx
+++ moveit-next/src/pages/index.tsx
@@ -1,9 +1,9 @@
-import { ExpirienceBar } from '../components/ExpirienceBar'
+import { ExperienceBar } from '../components/ExperienceBar'
 
 export default function Home() {
   return (
     <div className="container">
-      <ExpirienceBar />
+      <ExperienceBar />
     </div>
   )
 }
