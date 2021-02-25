42bc80f69c0cde8c419891fcb42ced762ee23e3b
samueljrz <samuelevangelista.ti@gmail.com>
2021-02-25 16:48:07 -0300

    using CountDownContext on aplication

--- moveit-next/src/components/ChallengeBox.tsx
+++ moveit-next/src/components/ChallengeBox.tsx
@@ -1,9 +1,21 @@
 import { useContext } from 'react';
 import { ChallengesContext } from '../contexts/ChallengesContext';
+import { CountDownContext } from '../contexts/CountDownContext';
 import styles from '../styles/components/ChallengeBox.module.css'
 
 export function ChallengeBox() { 
   const { activeChallenge, resetChallenge, completeChallenge } = useContext(ChallengesContext)
+  const { resetCountDown } = useContext(CountDownContext)
+
+  function handleChallengeCompleted() {
+    completeChallenge()
+    resetCountDown()
+  }
+
+  function handleChallengeFailed() {
+    resetChallenge()
+    resetCountDown()
+  }
 
   return(
     <div className={styles.challengeBoxContainer}>
@@ -22,14 +34,14 @@ export function ChallengeBox() {
             <button 
               type="button"
               className={styles.challengeFailedButton}
-              onClick={resetChallenge}
+              onClick={handleChallengeFailed}
             >
               Falhei
             </button>
             <button
               type="button"
               className={styles.challengeCompletedButton}
-              onClick={completeChallenge}
+              onClick={handleChallengeCompleted}
             >
               Completei
             </button>
